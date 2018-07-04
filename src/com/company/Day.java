6894493ed944940ea37c63157199347bbfb88458
Lakshmi Kalidindi <klakshmi75@gmail.com>
2018-07-04 16:11:44 +0530

    First commit with utility classes.

---
+++ src/com/company/Day.java
@@ -0,0 +1,74 @@
+package com.company;
+
+/**
+ * Created by lakshmik on 4/7/18.
+ */
+import java.time.LocalDate;
+import java.util.Date;
+import org.joda.time.DateTime;
+
+    public enum Day {
+
+        MONDAY(1,"M"),
+        TUESDAY(2,"Tu"),
+        WEDNESDAY(3,"W"),
+        THURSDAY(4,"Th"),
+        FRIDAY(5,"F"),
+        SATURDAY(6,"Sa"),
+        SUNDAY(7,"Su");
+
+        private int dayIndex;
+        private String shortName;
+
+        private Day(int day, String shortName){
+            this.dayIndex=day; this.shortName=shortName;
+        }
+
+        public int getDayIndex() {
+            return dayIndex;
+        }
+
+        public String getShortName() {
+            return shortName;
+        }
+
+        public static Day getDayFromString(String dayString){
+            for(Day day:Day.values()){
+                if(day.toString().equals(dayString)){
+                    return day;
+                }
+            }
+            return null;
+        }
+
+        public static Day getDayFromDayIndex(int dayIndex){
+            for(Day day:Day.values()){
+                if(day.getDayIndex()==dayIndex){
+                    return day;
+                }
+            }
+            return null;
+        }
+
+        public static Day getDayFromDate(Date previewDate){
+            return Day.getDayFromDayIndex((new DateTime(previewDate).getDayOfWeek()));
+        }
+
+        public static Day getDayFromLocalDate(LocalDate date){
+            return Day.getDayFromDayIndex(date.getDayOfWeek().getValue());
+        }
+
+        public int getWeekDayIndexForCron() {
+            return dayIndex == 7 ? 1 : dayIndex + 1;
+        }
+
+        public static Day getDayFromShortName(String shortName){
+            for(Day day:Day.values()){
+                if(day.getShortName().equals(shortName)){
+                    return day;
+                }
+            }
+            return null;
+        }
+
+}
---
+++ src/com/company/RecurringFrequency.java
@@ -0,0 +1,109 @@
+package com.company;
+
+import java.text.ParseException;
+import java.util.Date;
+import org.joda.time.Days;
+import org.joda.time.LocalDate;
+import play.libs.Time;
+
+/**
+ * Highest Priority indicated by Highest int value of the variable `priority`
+ */
+public enum RecurringFrequency {
+    EVERY_WEEK(1),
+    OTHER_WEEK(2),
+    FIRST_WEEK_OF_MONTH(3),
+    SECOND_WEEK_OF_MONTH(3),
+    THIRD_WEEK_OF_MONTH(3),
+    FOURTH_WEEK_OF_MONTH(3),
+    LAST_WEEK_OF_MONTH(4),
+    OCCURS_ONCE(5);
+
+    private int priority;
+
+    private RecurringFrequency(int priority) {
+        this.priority = priority;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public static boolean validate(RecurringFrequency recurringFrequency, Date eventStartDate, Date date) {
+        boolean result = false;
+        // Basic check, see if the week day of eventStartDate and the given date are same
+        if (Day.getDayFromDate(eventStartDate).getDayIndex() == new LocalDate(date).getDayOfWeek()) {
+            switch (recurringFrequency) {
+                case OCCURS_ONCE:
+                    result = validateOccursOnce(eventStartDate, date);
+                    break;
+                case LAST_WEEK_OF_MONTH:
+                case FIRST_WEEK_OF_MONTH:
+                case SECOND_WEEK_OF_MONTH:
+                case THIRD_WEEK_OF_MONTH:
+                case FOURTH_WEEK_OF_MONTH:
+                case EVERY_WEEK:
+                    result = validateSpecificWeekOfMonth(recurringFrequency, eventStartDate, date);
+                    break;
+                case OTHER_WEEK:
+                    result = validateOtherWeek(recurringFrequency, eventStartDate, date);
+                    break;
+                default:
+                    break;
+
+            }
+        } else {
+            System.out.println("Weekday of the event start date and the given date are not same");
+        }
+        return result;
+    }
+
+    private static boolean validateOccursOnce(Date eventStartDate, Date date) {
+        LocalDate jodaDate1 = new LocalDate(eventStartDate);
+        LocalDate jodaDate2 = new LocalDate(date);
+        return jodaDate1.isEqual(jodaDate2);
+    }
+
+    private static boolean validateSpecificWeekOfMonth(RecurringFrequency recurringFrequency, Date eventStartDate,
+                                                       Date date) {
+        StringBuilder cronExpression = new StringBuilder("* * * ? * ");
+        Day weekdayOfEventStartDate = Day.getDayFromDate(eventStartDate);
+        switch (recurringFrequency) {
+            case FIRST_WEEK_OF_MONTH:
+                cronExpression.append(weekdayOfEventStartDate.getWeekDayIndexForCron() + "#1");
+                break;
+            case SECOND_WEEK_OF_MONTH:
+                cronExpression.append(weekdayOfEventStartDate.getWeekDayIndexForCron() + "#2");
+                break;
+            case THIRD_WEEK_OF_MONTH:
+                cronExpression.append(weekdayOfEventStartDate.getWeekDayIndexForCron() + "#3");
+                break;
+            case FOURTH_WEEK_OF_MONTH:
+                cronExpression.append(weekdayOfEventStartDate.getWeekDayIndexForCron() + "#4");
+                break;
+            case LAST_WEEK_OF_MONTH:
+                cronExpression.append(weekdayOfEventStartDate.getWeekDayIndexForCron() + "L");
+                break;
+            case EVERY_WEEK:
+                cronExpression.append(weekdayOfEventStartDate.getWeekDayIndexForCron());
+
+                break;
+            default:
+                break;
+        }
+        try {
+            Time.CronExpression exp = new Time.CronExpression(cronExpression.toString());
+            return exp.isSatisfiedBy(date);
+        } catch (ParseException e) {
+            System.out.print("Some error in building cron expression" + cronExpression.toString());
+        }
+        return false;
+    }
+
+    // This method assumes that the eventStartDate is always the same weekday as the weekday of date to be validated.
+    private static boolean validateOtherWeek(RecurringFrequency recurringFrequency, Date eventStartDate, Date date) {
+        LocalDate jodaDate1 = new LocalDate(eventStartDate);
+        LocalDate jodaDate2 = new LocalDate(date);
+        return Days.daysBetween(jodaDate1, jodaDate2).getDays() % 14 == 0;
+    }
+}
